03cf6fef3d83e880e7e453d435c089977a8bcaa7
BernardoModerno <bernardo.moderno@gmail.com>
2021-12-30 18:57:07 -0300

    Tela de Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/Auth/SignIn';
 import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
+    <ChakraProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn/>} />
@@ -14,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home/>} />
       </Routes>
     </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -1,3 +1,58 @@
+import { AtSignIcon, LockIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, FormControl, FormHelperText, Heading, Image, Input, InputGroup, InputLeftElement, Link, Stack } from "@chakra-ui/react";
+import logo from "../../assets/logo.png";
+
 export default function SignIn() {
-    return <div>SignIn</div>;
+    return (
+        <Flex 
+          flexDirection="column"
+          width="100wh"
+          height="100vh"
+          backgroundColor="gray.200"
+          justifyContent="center"
+          alignItems="center"
+        > 
+            <Stack flexDir="column" mb="2" justifyContent="center" alignItems="center"> 
+                <Image src={logo} alt="Logo" boxSize="80px" objectFit="contain" />
+                <Heading>Cadastro de Clientes</Heading>
+                <Box minW={{ base: "90%", md: "470px" }}>
+                    <form>
+                        <Stack
+                            spacing={4}
+                            p="1rem"
+                            backgroundColor="whiteAlpha.900"
+                            boxShadow="md"
+                            borderRadius="md"
+                        >
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement 
+                                    pointerEvents="none"
+                                    children={<AtSignIcon color="gray.300" />}/>
+                                <Input type="email" placeholder="Endereço de email" />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement 
+                                    pointerEvents="none"
+                                    children={<LockIcon color="gray.300" />}/>
+                                <Input type="password" placeholder="Senha" />
+                                </InputGroup>
+                                <FormHelperText textAlign="right">
+                                    <Link>Esqueceu a senha?</Link>
+                                </FormHelperText>
+                            </FormControl>
+                            <Button type="submit" variant="solid" colorScheme="green">
+                                Login
+                            </Button>
+                        </Stack>
+                    </form>
+                </Box>
+            </Stack>
+            <Box>
+                Ainda não possui cadastro? <Link color="green">Cadastre-se</Link>
+            </Box>
+        </Flex>
+    );
 }
